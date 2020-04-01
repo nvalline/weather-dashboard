6c641c2aa3c8949d2155175b1ab735597cc1f8a2
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-04-01 11:31:09 -0600

    added print search history & reset history state

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,21 +2,33 @@ let searchLocations = [];
 
 $('#search-btn').on('click', function (event) {
     event.preventDefault();
-    let searchLocationValue = $('#search-input').val();
+    let searchLocationValue = $('#search-input').val().trim().toLowerCase();
     if (searchLocationValue === '') {
         return
+    } else if (searchLocations.includes(searchLocationValue)) {
+        // manipulate array for same value
+        console.log('includes value')
     } else {
         searchLocations.unshift(searchLocationValue)
     }
     printSearchLocations()
 })
 
+// add searches to search history list
 function printSearchLocations() {
+    resetLocationState()
     console.log(searchLocations)
     for (let i = 0; i < searchLocations.length; i++) {
         let location = searchLocations[i]
         let newLiElement = $('<li>').addClass('search-results')
         newLiElement.text(location)
-        $('#search-history-ul').prepend(newLiElement)
+        $('#search-history-ul').append(newLiElement)
+    }
+}
+
+// reset search history list
+function resetLocationState() {
+    if ($('#search-history-ul').has('li')) {
+        $('li').remove()
     }
 }
