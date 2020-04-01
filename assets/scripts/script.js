8ba69b577e957f4027badd6addea2bef07ef2298
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-04-01 12:29:39 -0600

    added local storage & retrieval

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,26 @@
 let searchLocations = [];
 
+init();
+
+// Click event for search button
 $('#search-btn').on('click', function (event) {
     event.preventDefault();
     let searchLocationValue = $('#search-input').val().trim().toLowerCase();
     if (searchLocationValue === '') {
         return
-    } else if (searchLocations.includes(searchLocationValue)) {
+    } else if (searchLocations != null && searchLocations.includes(searchLocationValue)) {
         // manipulate array for same value
         console.log('includes value')
     } else {
         searchLocations.unshift(searchLocationValue)
     }
     printSearchLocations()
+    storeLocations()
 })
 
 // add searches to search history list
 function printSearchLocations() {
     resetLocationState()
-    console.log(searchLocations)
     for (let i = 0; i < searchLocations.length; i++) {
         let location = searchLocations[i]
         let newLiElement = $('<li>').addClass('search-results')
@@ -31,4 +34,21 @@ function resetLocationState() {
     if ($('#search-history-ul').has('li')) {
         $('li').remove()
     }
+}
+
+function init() {
+    let storedLocations = localStorage.getItem('searchLocations')
+    let parsedLocations = JSON.parse(storedLocations)
+
+    if (parsedLocations != null) {
+        searchLocations = parsedLocations;
+        printSearchLocations();
+    } else {
+        searchLocations = [];
+    }
+}
+
+// save search locations to local storage
+function storeLocations() {
+    localStorage.setItem('searchLocations', JSON.stringify(searchLocations))
 }
